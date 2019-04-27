27332862d1627389992e890871dfad658e316b95
NikitaDyachkov <nikitadyachkovchik@gmail.com>
2019-04-27 11:12:25 +0300

    Add Class Action

--- Task №1/Task №1/Program.cs
+++ Task №1/Task №1/Program.cs
@@ -8,21 +8,20 @@ namespace Task__1
     {
         static void Main(string[] args)
         {
-            int numCase;
-            string quest;
+            Action action = new Action();
             while (true)
             {
                 Console.Write("1. Read txt file and delete the specified word in it \n" +
                             "2. Enter the number of words and output the text where after each 10th word there will be a comma \n" +
                          "3.Turn the words in the sentence \n4. Work with directories \n5.Exit \n nSelect the operation number->");
-                    quest = Console.ReadLine();
-                    if( int.TryParse(quest, out numCase))
+                string quest = Console.ReadLine();
+                    if( int.TryParse(quest, out int numCase))
                     switch (numCase)
                     {
-                        case 1: changeFile.change(); break;
-                        case 2: numberWords.number(); break;
-                        case 3: addArrayString.addArray(); break;
-                        case 4: DirectoryManager.getDir(); break;
+                        case 1: action.Change(); break;
+                        case 2: action.Number(); break;
+                        case 3: action.AddArray(); break;
+                        case 4: action.GetDir(); break;
                         case 5: return;
                         default: Console.WriteLine("\nInvalid input!!!\n"); ; break;
                     }
---
+++ Task №1/Task №1/Properties/Action.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Task__1
+{
+    class Action
+    {
+        
+        public Action() { }
+        public void GetDir()
+        {
+            string directory = @"C:\Users\Хозяйн";
+            string[] second = Directory.GetDirectories(directory);
+            Array.Sort(second);
+            for (int i = 0; i < second.Length; i++)
+            {
+                Console.WriteLine((i + 1) + "/////////" + second[i]);
+            }
+            Console.Write("\nEnter directory ID->");
+            string quest = Console.ReadLine();
+            if (int.TryParse(quest, out int numDirectory) && numDirectory < second.Length)
+            {
+                Console.WriteLine();
+                string[] second2 = Directory.GetFiles(second[numDirectory - 1]);
+                Array.Sort(second2);
+                foreach (string x in second2)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            else Console.WriteLine("There is no such directory.");
+
+        }
+        public void AddArray()
+        {
+            string text = FileManager.ReadFileString();
+            Console.WriteLine("What sentence do you want to flip");
+            string temp = Console.ReadLine();
+            Int32.TryParse(temp, out int sentenceNum);
+            string sentence = TextManager.ReverseSentence(TextManager.GetSentence(text, sentenceNum));
+            Console.WriteLine($"\n{sentence}\n");
+        }
+        public void Number()
+        {
+            string text = FileManager.ReadFileString();
+            Console.WriteLine($"\nNumber of words->{TextManager.GetWordsCount(text, out StringBuilder newline)}\n");
+            Console.WriteLine(newline + "\n");
+        }
+        public void Change()
+        {
+            string line = FileManager.ReadFileString();
+            Console.Write("\nEnter the word you want to delete. -> ");
+            string wordToDelete = Console.ReadLine();
+            bool quest = TextManager.ReplaceWords(line, wordToDelete);
+            if (!quest)
+                Console.WriteLine("\nThere is no such word\n");
+        }
+    }
+}
