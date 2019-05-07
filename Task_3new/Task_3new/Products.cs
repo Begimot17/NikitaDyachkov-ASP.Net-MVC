a051c71d6beaa9e27d99aafb388dac4889de21da
NikitaDyachkov <nikitadyachkovchik@gmail.com>
2019-05-07 19:33:10 +0300

    Task_3new

---
+++ Task_3new/Task_3new/Products.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Task_3new
+
+{
+    public class Products : IRepository
+    {
+        public string Name { get; set; }
+        public string Description{ get; set; }
+        public string Type{ get; set; }
+        public int Price{ get; set; }
+        string SortBy { get; set; }
+        public List<Products> ProdList ;
+        readonly string path = @"C:\Users\Хозяйн\Documents\asp.net-mvc repa\Task_3new\Task_3new\Xml\Product.xml";
+        public Products() { }
+        public Products(string Name, string Description, string Type, int Price)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Type = Type;
+            this.Price = Price;
+        }
+        public void AddProduct()
+        {
+            Update();
+            Products product = new Products();
+            Console.Write("Enter Name->");
+            product.Name = Console.ReadLine();
+            Console.Write("Enter Description->");
+            product.Description = Console.ReadLine();
+            Console.Write("Enter Type->");
+            product.Type = Console.ReadLine();
+            Console.Write("Enter Price->");
+            product.Price = Convert.ToInt16(Console.ReadLine());
+            XDocument xDoc = XDocument.Load(path);
+            XNode xNewNode = new XElement("Product", new XAttribute("Name", product.Name),
+                new XElement("Description", product.Description),
+                new XElement("Type", product.Type),
+                new XElement("Price", product.Price));
+            xDoc.Root.Add(xNewNode);
+            xDoc.Save(path);
+            Update();
+
+        }
+
+        public void Delete()
+        {
+            Update();
+            Console.WriteLine("Enter name delete");
+            string nameDelete = Console.ReadLine();
+            XDocument xDoc = XDocument.Load(path);
+            foreach (XElement xNode in xDoc.Root.Nodes())
+            {
+                if (xNode.Attribute("Name").Value == nameDelete)
+                {
+                    xNode.Remove();
+                }
+            }
+            xDoc.Save(path);
+            Update();
+
+        }
+
+        public void Search()
+        {
+            Update();
+            Console.WriteLine("Enter name to search");
+            string namesearch=Console.ReadLine();
+            foreach(Products x in ProductList())
+            {
+                if (x.Name == namesearch)
+                {
+                    Console.WriteLine("{0} {1} {2} {3}",x.Name, x.Description, x.Type, x.Price);
+                }
+            }
+
+        }
+        public void SortChange()
+        {
+            Update();
+            Console.WriteLine("Sort\n1=Name\n2=Description\n3=Type\n4=Price");
+            switch (Convert.ToInt16(Console.ReadLine()))
+            {
+                case 1: SortBy="Name"; break;
+                case 2: SortBy="Description"; break;
+                case 3: SortBy="Type"; break;
+                case 4: SortBy="Price"; break;
+            }
+            Sort();
+            
+        }
+        public void Sort()
+        {
+            switch (SortBy)
+            {
+                case "Name": ProdList = ProdList.OrderBy(x => x.Name).ToList(); break;
+                case "Description": ProdList = ProdList.OrderBy(x => x.Description).ToList(); break;
+                case "Type": ProdList = ProdList.OrderBy(x => x.Type).ToList(); break;
+                case "Price": ProdList = ProdList.OrderBy(x => x.Price).ToList(); break;
+            }
+            foreach (Products x in ProdList)
+            {
+                Console.WriteLine("{0,-24} {1,-10} {2,-10} {3,-10}", x.Name, x.Description, x.Type, x.Price);
+            }
+        }
+        public void Update()
+        {
+            ProdList = ProductList();
+        }
+
+        public List<Products> ProductList()
+        {
+            List<Products> product = new List<Products>();
+            XDocument xdoc = XDocument.Load(path);
+            foreach (XElement prod in xdoc.Element("Products").Elements("Product"))
+            {
+                XAttribute Name = prod.Attribute("Name");
+                XElement Description = prod.Element("Description");
+                XElement Type = prod.Element("Type");
+                XElement Price = prod.Element("Price");
+
+
+                if (Name != null && Description != null && Type != null&& Price != null)
+                {
+                    product.Add(new Products(Name.Value, Description.Value, Type.Value, Convert.ToInt32(Price.Value)));
+                }
+            }
+            return product;
+        }
+        public void CatalogShow()
+        {
+            Update();
+            foreach (Products x in ProdList)
+            {
+                Console.WriteLine("{0,-24} {1,-10} {2,-10} {3,-10}",x.Name, x.Description, x.Type, x.Price);
+            }
+        }
+        
+    }
+}
---
+++ Task_3new/Task_3new/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_3new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Products p = new Products();
+                Console.WriteLine("1=Registration\n2=Log in\n3=Products\n4=Exit");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1: Registration(); break;
+                    case 2: Login(); break;
+                    case 3: p.CatalogShow(); break;
+                    case 4: return;
+                    default: Console.WriteLine("Неверный ввод!!!"); break;
+                }
+            }
+
+        }
+        static void Registration()
+        {
+            while (true)
+            {
+                User newUser = new User();
+                Console.Write("Enter Name->");
+                newUser.Name = Console.ReadLine();
+                Console.Write("Enter Email->");
+                newUser.Email = Console.ReadLine();
+                Console.Write("Enter (min 6 chars) Password->");
+                newUser.Pass = Console.ReadLine();
+                bool name = Regex.IsMatch(newUser.Name, @"^[\p{L} \.\-]+$");
+                bool email = Regex.IsMatch(newUser.Email, @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+             @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+                bool pass = Regex.IsMatch(newUser.Pass, @"[0-9a-zA-Z!@#$%^&*]{6,}");
+                if (name && email && pass)
+                {
+                    foreach (User x in newUser.ListUsers())
+                    {
+                        if (x.Name == newUser.Name)
+                        {
+                            Console.WriteLine("A user with the same name already exists");
+                            break;
+                        }
+                        if (x.Email == newUser.Email)
+                        {
+                            Console.WriteLine("A user with the same email already exists");
+                            break;
+                        }
+                    }
+                    newUser.NewUser();
+                    UserConsole(newUser.Name);
+
+                }
+                else
+                {
+                    Console.WriteLine("WRONG ENTRY");
+                }
+            }
+        }
+        static void Login()
+        {
+            User temp = new User();
+            Console.Write("Enter Email->");
+            string Email = Console.ReadLine();
+            Console.Write("Enter (min 6 chars) Password->");
+            string Pass = Console.ReadLine();
+            if (Email == "Admin@gmail.com" && Pass == "123456")
+            {
+                AdminConsole();
+            }
+            else
+            {
+                foreach (User x in temp.ListUsers())
+                {
+                    if (x.Pass == Pass && x.Email == Email)
+                    {
+                        UserConsole(x.Name);
+                    }
+                }
+                Console.WriteLine("Invalid email or password");
+            }
+        }
+        static void UserConsole(string Name)
+        {
+            Cart cart = new Cart();
+            Console.WriteLine($"Hello {Name}");
+            while (true)
+            {
+                Console.WriteLine("1=CatalogShow\n2=CartShow\n3=AddProduct\n4=Delete\n5=Search\n6=Exit");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1: cart.CatalogShow(); break;
+                    case 2: cart.CartShow(Name); break;
+                    case 3: cart.AddProduct(Name); break;
+                    case 4: cart.Delete(Name); break;
+                    case 5: cart.Search(); break;
+                    case 6: return;
+                    default: Console.WriteLine("WRONG ENTRY!!!"); break;
+                }
+            }
+        }
+        static void AdminConsole()
+        {
+            Console.WriteLine("Hello Admin");
+            while (true)
+            {
+                Products prod = new Products();
+                Console.WriteLine("1=CatalogShow\n2=AddProduct\n3=Delete\n4=Sort\n5=Search\n6=Exit");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1: prod.CatalogShow(); break;
+                    case 2: prod.AddProduct(); break;
+                    case 3: prod.Delete(); break;
+                    case 4: prod.SortChange(); break;
+                    case 5: prod.Search(); break;
+                    case 6: return;
+                    default: Console.WriteLine("WRONG ENTRY!!!"); break;
+
+                }
+            }
+        }
+    }
+}
